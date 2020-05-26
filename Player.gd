75c51238b38db4982bab4aff45bc7c0bd075ff4f
Tom <11286187+Regular-Tom@users.noreply.github.com>
2020-05-26 23:41:15 +0700

    Make player controllable using mouse.

--- Player.gd
+++ Player.gd
@@ -1,4 +1,11 @@
 extends RigidBody2D
 
+export var launch_power: float = 500
+
+func launch(direction):
+	apply_central_impulse(direction * launch_power)
+
 func _input(event):
-	pass
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT:
+			launch((event.position - global_position).normalized())
