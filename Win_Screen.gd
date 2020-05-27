c4a44a7f96c840d5d64bfa7266024fb1aa0fb2ce
Tom <11286187+Regular-Tom@users.noreply.github.com>
2020-05-27 15:55:34 +0700

    Make losing player cry.

--- Win_Screen.gd
+++ Win_Screen.gd
@@ -4,7 +4,14 @@ func _ready():
 	if Winner.player == 1:
 		$Player_1_Win.show()
 		$Player_2_Win.hide()
-
+		$Player_1_Cry.hide()
+		$Player_2_Cry.show()
+	else:
+		$Player_1_Win.hide()
+		$Player_2_Win.show()
+		$Player_1_Cry.show()
+		$Player_2_Cry.hide()
+		
 func _input(event):
 	if event is InputEventKey:
 		if event.scancode in [KEY_ENTER, KEY_SPACE]:
