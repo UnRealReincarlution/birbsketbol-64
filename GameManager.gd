eae7c81f8d45a94103f261471e500142dd613048
Tom <11286187+Regular-Tom@users.noreply.github.com>
2020-05-27 15:27:39 +0700

    Add winning score to display.

--- GameManager.gd
+++ GameManager.gd
@@ -1,11 +1,14 @@
 extends Control
 
+export var winning_score = 3
+
 onready var score_player_1 = 0
 onready var score_player_2 = 0
 
 func _ready():
 	for goal in get_tree().get_nodes_in_group("goal"):
 		goal.connect("goal_scored", self, "on_goal_scored", [goal.player])
+	update_score_display()
 
 func on_goal_scored(player):
 	match player:
@@ -16,5 +19,5 @@ func on_goal_scored(player):
 	update_score_display()
 
 func update_score_display():
-	$Player_1_Score.text = str(score_player_1)
-	$Player_2_Score.text = str(score_player_2)
+	$Player_1_Score.text = "%d/%d" % [score_player_1, winning_score]
+	$Player_2_Score.text = "%d/%d" % [score_player_2, winning_score]
